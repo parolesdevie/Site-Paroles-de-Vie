85601308b6801c142c0b1eb9a57d8fef79a8feb8
parolesdevie <117495360+parolesdevie@users.noreply.github.com>
2022-11-06 05:31:48 +0100

    feat: add tag selection

--- js/main.js
+++ js/main.js
@@ -1,16 +1,45 @@
 document.addEventListener('DOMContentLoaded', () => {
   if (document.getElementById('home')) {
     const eInput = document.getElementById('search-input')
+    const ePdfList = document.getElementById('pdf-list').children
 
     eInput.addEventListener('input', () => {
       const word = slugify(eInput.value)
-      const list = document.getElementById('pdf-list').children
 
-      for (i = 0; i < list.length; i++) {
-        const { slug } = list[i].dataset
-        list[i].style.display = slug.indexOf(word) > -1 ? '' : 'none'
+      for (i = 0; i < ePdfList.length; i++) {
+        const { slug } = ePdfList[i].dataset
+        ePdfList[i].style.display = slug.indexOf(word) > -1 ? '' : 'none'
       }
     })
+
+    const eTags = document.getElementById('tag-list').children
+    const tagActifs = []
+
+    for (let i = 0; i < eTags.length; i++) {
+      const eTag = eTags[i]
+
+      eTag.addEventListener('click', () => {
+        const { value } = eTag.dataset
+        const index = tagActifs.indexOf(value)
+
+        if (index > -1) {
+          tagActifs.splice(index, 1)
+          eTag.classList.remove('tag-active')
+        } else {
+          tagActifs.push(value)
+          eTag.classList.add('tag-active')
+        }
+
+        for (i = 0; i < ePdfList.length; i++) {
+          const { tag } = ePdfList[i].dataset
+          if (tagActifs.length) {
+            ePdfList[i].style.display = tagActifs.includes(tag) ? '' : 'none'
+          } else {
+            ePdfList[i].style.display = ''
+          }
+        }
+      })
+    }
   }
 })
 
