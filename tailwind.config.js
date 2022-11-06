77debb3a02bf1407fe3ddc91a46d8dc44ca5b678
parolesdevie <117495360+parolesdevie@users.noreply.github.com>
2022-11-06 01:41:26 +0100

    feat: first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+'use strict'
+
+module.exports = {
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      spacing: {
+        // 120: '30rem',
+        // 150: '37.5rem'
+      }
+    }
+  },
+  variants: {
+    extend: {
+      // opacity: ['hover']
+    }
+  }
+}
