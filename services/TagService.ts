af61ff5590d69b2fe1452b03a6c5ee56a741fd37
parolesdevie <chaine.paroles.de.vie@gmail.com>
2022-11-16 09:27:22 +0100

    fix: fix Itag enum

--- services/TagService.ts
+++ services/TagService.ts
@@ -1,12 +1,11 @@
-import { ITag } from "~/types";
+import { ITag } from '~/types'
 
 export default class TagService {
-    constructor() {
-    }
+  constructor() {}
 
-    static getAll(): ITag[] {
-        return [
-            {
+  static getAll(): ITag[] {
+    return [
+      {
         slug: 'coliseum',
         author: 'Coliseum',
         pdfCount: 2,
@@ -22,45 +21,45 @@ export default class TagService {
         pdfCount: 45,
       },
       {
-                slug: 'jozua',
-                author: 'Jozua',
-                pdfCount: 45
-            },
-            {
-                slug: 'jms',
-                author: 'Jms',
-                pdfCount: 11
-            },
-            {
-                slug: 'fidelis-and-verax',
-                author: 'Fidelis & Verax',
-                pdfCount: 4
-            },
-            {
-                slug: 'benevolence',
-                author: 'Benevolence',
-                pdfCount: 3
-            },
-            {
-                slug: 'sham',
-                author: 'Sham',
-                pdfCount: 2
-            },
-            {
-                slug: 'qalev',
-                author: 'Qalev',
-                pdfCount: 2
-            },
-            {
-                slug: 'saved-by-christ',
-                author: 'Saved By Christ',
-                pdfCount: 1
-            },
-            {
-                slug: 'protocole',
-                author: 'Protocole',
-                pdfCount: 1
-            },
-        ]
-    }
+        slug: 'jozua',
+        author: 'Jozua',
+        pdfCount: 45,
+      },
+      {
+        slug: 'jms',
+        author: 'Jms',
+        pdfCount: 11,
+      },
+      {
+        slug: 'fidelis-and-verax',
+        author: 'Fidelis & Verax',
+        pdfCount: 4,
+      },
+      {
+        slug: 'benevolence',
+        author: 'Benevolence',
+        pdfCount: 3,
+      },
+      {
+        slug: 'sham',
+        author: 'Sham',
+        pdfCount: 2,
+      },
+      {
+        slug: 'qalev',
+        author: 'Qalev',
+        pdfCount: 2,
+      },
+      {
+        slug: 'saved-by-christ',
+        author: 'Saved By Christ',
+        pdfCount: 1,
+      },
+      {
+        slug: 'protocole',
+        author: 'Protocole',
+        pdfCount: 1,
+      },
+    ]
+  }
 }
--- types/index.ts
+++ types/index.ts
@@ -1,28 +1,29 @@
 export interface ITag {
-    slug: string;
-    author: string;
-    pdfCount: Number;
+  slug: string
+  author: string
+  pdfCount: Number
 }
 
 export interface IPdf {
-    author: string;
-    slug: string;
-    title: string;
+  tags: string[]
+  author: string
+  slug: string
+  title: string
 }
 
 export interface IWebSite {
-    name: string;
-    href: string;
+  name: string
+  href: string
 }
 
 export interface IBook {
-    title: string;
-    slug: string;
+  title: string
+  slug: string
 }
 
 export interface ILink {
-    name: string;
-    to: string;
+  name: string
+  to: string
 }
 
 export type IBreadcrumbItem = ILink
