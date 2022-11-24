8f81cb27ff400bf1c494bcd53c562be4fb24f97a
parolesdevie <chaine.paroles.de.vie@gmail.com>
2022-11-24 18:26:05 +0100

    feat add manuscripts page

---
+++ pages/manuscripts.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="flex flex-col min-h-screen">
+    <!-- header -->
+    <Header />
+    <div class="container mx-auto mb-auto px-3 md:px-0 py-4 flex flex-col">
+      <Breadcrumb :items="breadcrumbItems" />
+
+      <!-- links -->
+      <nav class="mt-2 md:mt-4">
+        <ul class="flex flex-col leading-7">
+          <li v-for="(webSite, index) in sites" :key="index">
+            <a
+              class="flex items-center hover:text-blue-700"
+              :href="webSite.href"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span class="block h-1.5 w-1.5 rounded-full bg-current mr-2" />
+              <span>
+                {{ webSite.name }}
+              </span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+
+    <!-- footer -->
+    <Footer class="mt-40" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Breadcrumb from '~/components/global/Breadcrumb.vue'
+import { WebSiteService } from '~/services'
+
+export default Vue.extend({
+  name: 'MagazinesPage',
+
+  components: { Breadcrumb },
+
+  data() {
+    return {
+      breadcrumbItems: [
+        {
+          name: 'Home',
+          to: '/',
+        },
+        {
+          name: 'Manuscrits',
+          to: '/manuscripts/',
+        },
+      ],
+      sites: WebSiteService.getManuscripts(),
+    }
+  },
+})
+</script>
--- services/WebSiteService.ts
+++ services/WebSiteService.ts
@@ -1,35 +1,63 @@
-import { IWebSite } from "~/types";
+import { IWebSite } from '~/types'
 
 export default class WebSiteService {
-    constructor() {
-    }
+  constructor() {}
 
-    static getAll(): IWebSite[] {
-        return [
-            {
-                name: 'Somme théologique saint thomas d’aquin (Français)',
-                href: 'http://docteurangelique.free.fr/livresformatweb/sommes/1sommetheologique1apars.htm',
-            },
-            {
-                name: "Exégèse des pères de l'église sur l'évangile (Français)",
-                href: 'https://www.apologetique.net/EvangilePere/EvangilePere.aspx',
-            },
-            {
-                name: 'Chaîne YouTube BibleProject (Français)',
-                href: 'https://www.youtube.com/c/BibleProjectFran%C3%A7ais',
-            },
-            {
-                name: 'Écrits des peres de l’Église (Anglais)',
-                href: 'https://www.newadvent.org/fathers/',
-            },
-            {
-                name: 'Sources Chrétiennes',
-                href: 'https://sourceschretiennes.org/',
-            },
-            {
-                name: 'jesusmarie',
-                href: 'http://jesusmarie.free.fr/',
-            },
-        ]
-    }
+  static getAll(): IWebSite[] {
+    return [
+      {
+        name: 'Codex Sinaiticus',
+        href: 'https://codexsinaiticus.org/en/',
+      },
+      {
+        name: 'Codex Alexandrinus',
+        href: 'https://manuscripts.csntm.org/manuscript/View/GA_02',
+      },
+      {
+        name: 'Digital Vatican Library',
+        href: 'https://digi.vatlib.it/mss/',
+      },
+      {
+        name: 'Somme théologique saint thomas d’aquin (Français)',
+        href: 'http://docteurangelique.free.fr/livresformatweb/sommes/1sommetheologique1apars.htm',
+      },
+      {
+        name: "Exégèse des pères de l'église sur l'évangile (Français)",
+        href: 'https://www.apologetique.net/EvangilePere/EvangilePere.aspx',
+      },
+      {
+        name: 'Chaîne YouTube BibleProject (Français)',
+        href: 'https://www.youtube.com/c/BibleProjectFran%C3%A7ais',
+      },
+      {
+        name: 'Écrits des peres de l’Église (Anglais)',
+        href: 'https://www.newadvent.org/fathers/',
+      },
+      {
+        name: 'Sources Chrétiennes',
+        href: 'https://sourceschretiennes.org/',
+      },
+      {
+        name: 'jesusmarie',
+        href: 'http://jesusmarie.free.fr/',
+      },
+    ]
+  }
+
+  static getManuscripts(): IWebSite[] {
+    return [
+      {
+        name: 'Codex Sinaiticus',
+        href: 'https://codexsinaiticus.org/en/',
+      },
+      {
+        name: 'Codex Alexandrinus',
+        href: 'https://manuscripts.csntm.org/manuscript/View/GA_02',
+      },
+      {
+        name: 'Digital Vatican Library',
+        href: 'https://digi.vatlib.it/mss/',
+      },
+    ]
+  }
 }
