b2abf4657b7fbef2488afef02ae50a9c025649a1
parolesdevie <chaine.paroles.de.vie@gmail.com>
2022-12-10 11:58:36 +0100

    feat: add scroll to top button

--- components/global/Footer.vue
+++ components/global/Footer.vue
@@ -1,5 +1,33 @@
 <template>
   <footer class="px-3 md:px-0 py-4 md:py-12 bg-black">
+    <transition name="fade">
+      <span
+        tabindex="0"
+        id="pagetop"
+        class="
+          cursor-pointer
+          fixed
+          right-8
+          bottom-8
+          h-12
+          w-12
+          md:h-14 md:w-14
+          bg-orange-500
+          text-white
+          rounded-full
+          flex
+          justify-center
+          items-center
+        "
+        v-show="scY > 300"
+        @keypress.space.prevent="toTop"
+        @keypress.enter="toTop"
+        @click="toTop"
+      >
+        <IconChevronUp class="h-7 w-7 md:h-8 md:w-8" />
+      </span>
+    </transition>
+
     <div class="container mx-auto flex flex-col md:flex-row gap-10">
       <!-- internal links -->
       <div class="flex flex-col mb-4" v-if="linksMainMenu.length">
@@ -13,7 +41,13 @@
           >
             <li v-for="(link, index) in linksMainMenu" :key="index">
               <NuxtLink
-                class="flex items-center hover:text-blue-500 text-xs md:text-base"
+                class="
+                  flex
+                  items-center
+                  hover:text-blue-500
+                  text-xs
+                  md:text-base
+                "
                 :to="link.to"
               >
                 <span>
@@ -37,7 +71,13 @@
           >
             <li v-for="(link, index) in linksMenuDiscord" :key="index">
               <NuxtLink
-                class="flex items-center hover:text-blue-500 text-xs md:text-base"
+                class="
+                  flex
+                  items-center
+                  hover:text-blue-500
+                  text-xs
+                  md:text-base
+                "
                 :to="link.to"
               >
                 <span>
@@ -55,15 +95,41 @@
 <script lang="ts">
 import Vue from 'vue'
 import { MenuService } from '~/services'
+import IconChevronUp from '~/components/icons/IconChevronUp.vue'
 
 export default Vue.extend({
   name: 'Footer',
+
+  components: { IconChevronUp },
 
   data() {
     return {
       linksMainMenu: MenuService.getMainMenu(),
       linksMenuDiscord: MenuService.getDiscordMenu(),
+      scTimer: 0,
+      scY: 0,
     }
+  },
+
+  mounted() {
+    window.addEventListener('scroll', this.handleScroll)
+  },
+
+  methods: {
+    handleScroll: function () {
+      if (this.scTimer) return
+      this.scTimer = setTimeout(() => {
+        this.scY = window.scrollY
+        clearTimeout(this.scTimer)
+        this.scTimer = 0
+      }, 100)
+    },
+    toTop: function () {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      })
+    },
   },
 })
 </script>
