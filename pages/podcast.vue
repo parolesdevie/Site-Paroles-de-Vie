32cb921e184d44e04df3be3803864c8efe7ac9c1
parolesdevie <chaine.paroles.de.vie@gmail.com>
2022-12-21 21:57:56 +0100

    feat: update podcast page with 2 embed (free/premium)

--- pages/podcast.vue
+++ pages/podcast.vue
@@ -9,49 +9,87 @@
       <section class="container mx-auto mt-4 md:mt-10">
         <!-- title -->
         <h2 class="flex items-center font-bold text-2xl md:text-3xl">
-          Spotify
+          Ecouter depuis Spotify
         </h2>
 
-        <!-- embed spotify -->
-        <iframe
-          class="mt-4 md:mt-10 w-full"
-          src="https://open.spotify.com/embed/show/5XnBOuPu5fUW0DROVinvbg?utm_source=generator&theme=0"
-          height="352"
-          frameBorder="0"
-          allowfullscreen=""
-          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-          loading="lazy"
-        />
+        <!-- embeds spotify -->
+        <div class="mt-4 md:mt-10 flex flex-col md:flex-row gap-5">
+          <article class="flex-1 flex flex-col">
+            <h2 class="flex items-center">
+              Ecouter Le dernier podcast gratuit
+              <!-- <IconLockOpen class="ml-2 h-4" /> -->
+            </h2>
+            <iframe
+              class="mt-2 md:mt-4 w-full"
+              src="https://open.spotify.com/embed/episode/2NO0OpMyay5TbZZpMPS66g?utm_source=generator&theme=0"
+              height="152"
+              frameBorder="0"
+              allowfullscreen=""
+              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+              loading="lazy"
+            />
+          </article>
+          <article class="flex-1 flex flex-col">
+            <h2 class="flex items-center">
+              Ecouter Le dernier podcast (Premium
+              <IconLock class="ml-2 h-3 md:h-4" />
+              )
+            </h2>
+            <iframe
+              class="mt-2 md:mt-4 w-full"
+              src="https://open.spotify.com/embed/show/5XnBOuPu5fUW0DROVinvbg?utm_source=generator&theme=0"
+              height="152"
+              frameBorder="0"
+              allowfullscreen=""
+              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+              loading="lazy"
+            />
+          </article>
+        </div>
       </section>
 
-      <section class="container mx-auto mt-4 md:mt-10">
-        <!-- title -->
-        <h2 class="flex items-center font-bold text-2xl md:text-3xl">Anchor</h2>
-
-        <!-- embed anchor -->
-        <iframe
-          class="mt-4 md:mt-10 w-full md:w-120"
-          src="https://anchor.fm/paroles-de-vie/embed/episodes/Le-symbole-des-aptres--mon-CREDO-e1sgrmu"
-          height="102px"
-          loading="lazy"
-          scrolling="no"
-        />
-      </section>
-
+      <!-- extern plateforme -->
       <section class="container mx-auto mt-4 md:mt-10">
         <!-- title -->
         <h2 class="flex items-center font-bold text-2xl md:text-3xl">
-          Amazon Music
+          Ecouter sur une autre plateforme
         </h2>
 
-        <a
-          class="mt-4 md:mt-10 inline-block"
-          href="https://music.amazon.fr/podcasts/0a53af38-888e-43d8-a6b3-3a4fd5255955/paroles-de-vie"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Ecouter sur Amazon Music
-        </a>
+        <ul class="mt-4 md:mt-10 flex gap-10 overflow-x-auto">
+          <li class="contents">
+            <a
+              class="flex flex-col items-center"
+              href="https://music.amazon.fr/podcasts/0a53af38-888e-43d8-a6b3-3a4fd5255955/paroles-de-vie"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <IconAmazon class="h-20" />
+              <span class="mt-2">Ecouter sur Amazon Music</span>
+            </a>
+          </li>
+          <li class="contents">
+            <a
+              class="flex flex-col items-center"
+              href="https://open.spotify.com/show/5XnBOuPu5fUW0DROVinvbg?go=1&sp_cid=b07c80b40af1cad5b9c13cec447d0ccb&utm_source=embed_player_p&utm_medium=desktop&nd=1"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <IconSpotify class="h-20" />
+              <span class="mt-2">Ecouter sur Spotify</span>
+            </a>
+          </li>
+          <li class="contents">
+            <a
+              class="flex flex-col items-center"
+              href="https://anchor.fm/paroles-de-vie"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <IconAnchor class="h-20" />
+              <span class="mt-2">Ecouter sur Anchor</span>
+            </a>
+          </li>
+        </ul>
       </section>
 
       <a
@@ -72,14 +110,27 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import IconAmazon from '~/components/icons/IconAmazon.vue'
+import IconSpotify from '~/components/icons/IconSpotify.vue'
+import IconAnchor from '~/components/icons/IconAnchor.vue'
 import IconRssSquare from '~/components/icons/IconRssSquare.vue'
+import IconLock from '~/components/icons/IconLock.vue'
+import IconLockOpen from '~/components/icons/IconLockOpen.vue'
 import SectionIntroTopics from '~/components/topics/SectionIntroTopics.vue'
 import { TopicService } from '~/services'
 
 export default Vue.extend({
   name: 'PodcastPage',
 
-  components: { SectionIntroTopics, IconRssSquare },
+  components: {
+    SectionIntroTopics,
+    IconRssSquare,
+    IconAmazon,
+    IconSpotify,
+    IconAnchor,
+    IconLock,
+    IconLockOpen,
+  },
 
   head() {
     return {
