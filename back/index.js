38e08478f34f3db28699cc8209a16b4170ddd1fd
parolesdevie <chaine.paroles.de.vie@gmail.com>
2022-12-29 18:23:00 +0100

    feat: add basique chat io

--- back/index.js
+++ back/index.js
@@ -1,10 +1,20 @@
 const express = require('express')
+const app = express()
+const path = require('path')
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+const port = process.env.PORT || 3000
+
 const cors = require('cors')
 const axios = require('axios').default
 const FormData = require('form-data')
 const cheerio = require('cheerio')
-const app = express()
-const port = 3000
+
+server.listen(port, () => {
+  console.log(`http://localhost:${port}/`)
+})
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(cors({ origin: '*' }))
 
@@ -148,6 +158,62 @@ app.get('/searchOnAleteia/', async (req, res) => {
   )
 })
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/search/`)
+let numUsers = 0
+
+io.on('connection', (socket) => {
+  let addedUser = false
+
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', (data) => {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    })
+  })
+
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', (username) => {
+    if (addedUser) return
+
+    // we store the username in the socket session for this client
+    socket.username = username
+    ++numUsers
+    addedUser = true
+    socket.emit('login', {
+      numUsers: numUsers
+    })
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    })
+  })
+
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', () => {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    })
+  })
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', () => {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    })
+  })
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', () => {
+    if (addedUser) {
+      --numUsers
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      })
+    }
+  })
 })
---
+++ front/pages/chat.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="flex flex-col min-h-screen">
+    <!-- header -->
+    <Header />
+    <div class="container mx-auto mb-auto px-3 md:px-0 py-4 flex flex-col">
+      <!-- breadcrumb -->
+      <Breadcrumb :items="breadcrumbItems" />
+
+      <section class="container mx-auto mt-4 md:mt-10">
+        <iframe
+          class="h-160 w-full"
+          :src="API_ENDPOINT + '/index.html'"
+          frameBorder="0"
+        />
+      </section>
+    </div>
+
+    <!-- footer -->
+    <Footer class="mt-40" />
+  </div>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: 'Messagerie instantanée - Paroles de vie',
+  meta: [
+    {
+      hid: 'description',
+      name: 'description',
+      content:
+        'Défendre la foi chrétienne, donner une éducation chrétienne, expression de la foi.'
+    },
+    {
+      hid: 'og:image',
+      property: 'og:image',
+      content: 'https://www.paroles-de-vie.tk/seo/chat.webp'
+    },
+    {
+      hid: 'twitter:image',
+      property: 'twitter:image',
+      content: 'https://www.paroles-de-vie.tk/seo/chat.webp'
+    }
+  ]
+})
+</script>
+
+<script lang="ts">
+export default defineNuxtComponent({
+  name: 'DivinityOfJesusPage',
+
+  components: {},
+
+  data() {
+    return {
+      breadcrumbItems: [
+        {
+          name: 'Accueil',
+          to: '/'
+        },
+        {
+          name: 'Messagerie instantanée',
+          to: '/chat/'
+        }
+      ],
+      API_ENDPOINT: 'http://178.170.13.79:8081'
+    }
+  }
+})
+</script>
