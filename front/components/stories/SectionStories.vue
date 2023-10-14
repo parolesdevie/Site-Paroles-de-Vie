5e9b5221578d13350250b0de945cd6705fd2834a
parolesdevie <chaine.paroles.de.vie@gmail.com>
2023-10-14 10:07:19 +0200

    feat: add storie circle responsive image

--- front/components/stories/SectionStories.vue
+++ front/components/stories/SectionStories.vue
@@ -10,9 +10,9 @@ const stories = useState(() => StoriesService.getAll())
       <StoriesSlideShow @close="handleCloseEvent" :story="currentStory" />
     </div>
 
-    <ul class="flex overflow-x-auto gap-1 md:gap-2 pb-4">
+    <ul class="flex overflow-x-auto md:gap-2 pb-4">
       <li
-        class="w-28 md:w-32 flex flex-col items-center cursor-pointer rounded-xl p-2 md:p-4 shrink-0"
+        class="w-20 md:w-32 flex flex-col items-center cursor-pointer rounded-xl md:p-4 shrink-0"
         tabindex="0"
         @keydown.esc="handleOpenEvent(undefined)"
         v-for="(story, index) in stories"
@@ -23,14 +23,39 @@ const stories = useState(() => StoriesService.getAll())
           class="border-2 border-gray-200 dark:border-gray-700 rounded-full p-1"
         >
           <div class="rounded-full" :style="{ backgroundColor: story.color }">
-            <img
-              class="h-16 w-16 md:h-20 md:w-20 rounded-full animate-fade"
-              :src="`/images/stories/${story.slug}/cover.jpg`"
-              :alt="story.alt"
-            />
+            <picture>
+              <source
+                height="80"
+                width="80"
+                media="(min-width: 768px)"
+                type="image/webp"
+                :srcset="`/images/stories/${story.slug}/desktop/cover@1x.webp,
+                          /images/stories/${story.slug}/desktop/cover@1.5x.webp 1.5x,
+                          /images/stories/${story.slug}/desktop/cover@2x.webp 2x,
+                          /images/stories/${story.slug}/desktop/cover@3x.webp 3x,
+                          /images/stories/${story.slug}/desktop/cover@4x.webp 4x`"
+              />
+              <source
+                height="56"
+                width="56"
+                type="image/webp"
+                :srcset="`/images/stories/${story.slug}/mobile/cover@1x.webp,
+                          /images/stories/${story.slug}/mobile/cover@1.5x.webp 1.5x,
+                          /images/stories/${story.slug}/mobile/cover@2x.webp 2x,
+                          /images/stories/${story.slug}/mobile/cover@3x.webp 3x,
+                          /images/stories/${story.slug}/mobile/cover@4x.webp 4x`"
+              />
+              <img
+                height="80"
+                width="80"
+                class="h-14 w-14 md:h-20 md:w-20 rounded-full animate-fade"
+                :src="`/images/stories/${story.slug}/desktop/cover@1x.webp`"
+                :alt="story.alt"
+              />
+            </picture>
           </div>
         </div>
-        <p class="mt-3 font-medium text-center text-sm md:text-base">
+        <p class="mt-3 font-medium text-center text-xs md:text-base">
           {{ story.title }}
         </p>
       </li>
